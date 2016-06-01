7edb03e7efb5f811c7d7bdf930f2db49e5e56b6e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-06-01 13:53:11 -0400

    updated contact.html and app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -18,6 +18,40 @@
     // define your paragraphs array;
     var paragraphs = [];
     
+    // create a reference to the sendButton
+    var sendButton = document.getElementById("sendButton");
+    
+    // event listener
+    sendButton.addEventListener("click", sendButtonClick);
+    
+    // event handler function
+    function sendButtonClick(event) {
+        console.log("clicked!");
+    }
+    
+    // create a reference to the firstName field
+     var firstName = document.getElementById("firstName");
+    
+    // create a reference to the form
+    var contactForm = document.getElementById("contactForm");
+    
+    // event listener with inline anonymous event handler function
+    contactForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        console.log("submitted");
+        showFormInput();
+        contactForm.reset();
+    });
+    
+    function showFormInput() {
+        console.log("++++++++++++++++++++++++++++++++");
+        console.log("First Name: " + firstName.value);
+        console.log("++++++++++++++++++++++++++++++++");
+    }
+    
+   
+    
+    
     // data for my pages
     paragraphs[0] = "<span class='firstSentence'>This is my first paragraph.</span> It is only visible on the first page. This next sentence is to prove that this really works!!! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla iaculis venenatis eros nec viverra. Aenean egestas sit amet dui eget ultrices. Ut vitae ullamcorper risus. Aenean risus eros, laoreet eu luctus pharetra, luctus et massa. Cras ullamcorper erat in justo malesuada pulvinar. Ut ac sollicitudin lorem, ut mattis ipsum. Maecenas commodo efficitur quam, et suscipit eros faucibus id. Suspendisse ut urna odio. Morbi euismod nec dui vel tempor. Ut vehicula libero odio, vel tempus eros scelerisque non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras efficitur porta mauris ac aliquet. Curabitur et sollicitudin ex.";
     paragraphs[1] = "<span class='firstSentence'>This is my second paragraph.</span> It is only visible on the second page. Nam commodo sodales porttitor. Proin sed purus ex. Sed id tortor massa. Vestibulum gravida sollicitudin accumsan. Proin ultricies ornare quam in feugiat. In mattis lorem at felis commodo, vel pellentesque ante varius. Cras rutrum efficitur bibendum. In sit amet ante est. Curabitur ullamcorper, enim vel aliquam pretium, quam ex tristique lacus, sed dapibus sapien est quis tellus. Cras rhoncus tempus dui a porta. Quisque est risus, vulputate vel ligula non, elementum bibendum neque. Vestibulum vel tellus porta, laoreet lacus nec, porttitor sapien. Proin ex metus, vehicula vitae nisi non, efficitur ultrices urna.";
